9855d1e723601a940e2fbd025c2ef64b69ca39cc
Jobson Batista <jobson.silva@dcx.ufpb.br>
2021-09-11 08:07:56 -0300

    Configurando repositório

---
+++ snet/src/test/java/com/snet/snet/SnetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snet.snet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
